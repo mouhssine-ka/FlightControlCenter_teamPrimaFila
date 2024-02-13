9937b3c9ddb74b702c9f81a304c971dbdeb828dd
luca savoretti <luca.savoretti@4ts.it>
2024-02-13 12:47:01 +0100

    aggiunto indicazione della flotta selezionata nella main

--- FlightSimulatorControlCenter/MainWindow.cs
+++ FlightSimulatorControlCenter/MainWindow.cs
@@ -18,11 +18,18 @@ namespace FlightSimulatorControlCenter
 
         private string NomeFlottaSelezionata = "Flotta Rayanair";
         private long IdFlottaSelezionata = 0;
+        private ToolStripLabel LabelFlottaSelezionata;
 
         public MainWindow(IValidationUserInputService validationService)
         {
             InitializeComponent();
             _validationService = validationService;
+
+            LabelFlottaSelezionata = new ToolStripLabel(NomeFlottaSelezionata);
+            LabelFlottaSelezionata.Alignment = ToolStripItemAlignment.Right;
+            LabelFlottaSelezionata.Padding = new Padding(0,0,20,0);
+
+            menuStrip1.Items.Add(LabelFlottaSelezionata);           
         }
 
         private void airplaneManager_Click(object sender, EventArgs e)
