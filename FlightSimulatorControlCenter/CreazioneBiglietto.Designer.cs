78ff22c54582046b0977c92a3e5b5ebeb995e251
Federico Grassi <grfederico10@gmail.com>
2024-03-01 14:13:55 +0100

    Modificate form Biglietto

--- FlightSimulatorControlCenter/CreazioneBiglietto.Designer.cs
+++ FlightSimulatorControlCenter/CreazioneBiglietto.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CreazioneBiglietto";
+            CercaVoloPerBiglietto_button = new Button();
+            Annulla_button = new Button();
+            CreaBiglietto_button = new Button();
+            DatiVolo_label = new Label();
+            SuspendLayout();
+            // 
+            // CercaVoloPerBiglietto_button
+            // 
+            CercaVoloPerBiglietto_button.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            CercaVoloPerBiglietto_button.Location = new Point(23, 24);
+            CercaVoloPerBiglietto_button.Name = "CercaVoloPerBiglietto_button";
+            CercaVoloPerBiglietto_button.Size = new Size(199, 64);
+            CercaVoloPerBiglietto_button.TabIndex = 9;
+            CercaVoloPerBiglietto_button.Text = "Cerca Volo";
+            CercaVoloPerBiglietto_button.UseVisualStyleBackColor = true;
+            // 
+            // Annulla_button
+            // 
+            Annulla_button.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Annulla_button.Location = new Point(163, 374);
+            Annulla_button.Name = "Annulla_button";
+            Annulla_button.Size = new Size(199, 64);
+            Annulla_button.TabIndex = 10;
+            Annulla_button.Text = "Annulla";
+            Annulla_button.UseVisualStyleBackColor = true;
+            // 
+            // CreaBiglietto_button
+            // 
+            CreaBiglietto_button.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            CreaBiglietto_button.Location = new Point(425, 374);
+            CreaBiglietto_button.Name = "CreaBiglietto_button";
+            CreaBiglietto_button.Size = new Size(199, 64);
+            CreaBiglietto_button.TabIndex = 11;
+            CreaBiglietto_button.Text = "Crea Biglietto";
+            CreaBiglietto_button.UseVisualStyleBackColor = true;
+            // 
+            // DatiVolo_label
+            // 
+            DatiVolo_label.AutoSize = true;
+            DatiVolo_label.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            DatiVolo_label.Location = new Point(23, 103);
+            DatiVolo_label.Name = "DatiVolo_label";
+            DatiVolo_label.Size = new Size(129, 28);
+            DatiVolo_label.TabIndex = 12;
+            DatiVolo_label.Text = "Dati del Volo:";
+            // 
+            // CreazioneBiglietto
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(DatiVolo_label);
+            Controls.Add(CreaBiglietto_button);
+            Controls.Add(Annulla_button);
+            Controls.Add(CercaVoloPerBiglietto_button);
+            Name = "CreazioneBiglietto";
+            Text = "CreazioneBiglietto";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button CercaVoloPerBiglietto_button;
+        private Button Annulla_button;
+        private Button CreaBiglietto_button;
+        private Label DatiVolo_label;
     }
 }
---
+++ FlightSimulatorControlCenter/SelezioneAereoPerBiglietto.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FlightSimulatorControlCenter
+{
+    partial class SelezioneAereoPerBiglietto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListaAereiPerBiglietto_table = new DataGridView();
+            SelezioneVolo_button = new Button();
+            Annulla_button = new Button();
+            ((System.ComponentModel.ISupportInitialize)ListaAereiPerBiglietto_table).BeginInit();
+            SuspendLayout();
+            // 
+            // ListaAereiPerBiglietto_table
+            // 
+            ListaAereiPerBiglietto_table.AllowUserToAddRows = false;
+            ListaAereiPerBiglietto_table.AllowUserToDeleteRows = false;
+            ListaAereiPerBiglietto_table.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            ListaAereiPerBiglietto_table.Location = new Point(12, 134);
+            ListaAereiPerBiglietto_table.Margin = new Padding(3, 4, 3, 4);
+            ListaAereiPerBiglietto_table.MultiSelect = false;
+            ListaAereiPerBiglietto_table.Name = "ListaAereiPerBiglietto_table";
+            ListaAereiPerBiglietto_table.ReadOnly = true;
+            ListaAereiPerBiglietto_table.RowHeadersWidth = 51;
+            ListaAereiPerBiglietto_table.Size = new Size(776, 303);
+            ListaAereiPerBiglietto_table.TabIndex = 1;
+            // 
+            // SelezioneVolo_button
+            // 
+            SelezioneVolo_button.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SelezioneVolo_button.Location = new Point(603, 42);
+            SelezioneVolo_button.Margin = new Padding(3, 4, 3, 4);
+            SelezioneVolo_button.Name = "SelezioneVolo_button";
+            SelezioneVolo_button.Size = new Size(153, 56);
+            SelezioneVolo_button.TabIndex = 3;
+            SelezioneVolo_button.Text = "Seleziona";
+            SelezioneVolo_button.UseVisualStyleBackColor = true;
+            // 
+            // Annulla_button
+            // 
+            Annulla_button.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Annulla_button.Location = new Point(38, 42);
+            Annulla_button.Margin = new Padding(3, 4, 3, 4);
+            Annulla_button.Name = "Annulla_button";
+            Annulla_button.Size = new Size(153, 56);
+            Annulla_button.TabIndex = 4;
+            Annulla_button.Text = "Annulla";
+            Annulla_button.UseVisualStyleBackColor = true;
+            // 
+            // SelezioneAereoPerBiglietto
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Annulla_button);
+            Controls.Add(SelezioneVolo_button);
+            Controls.Add(ListaAereiPerBiglietto_table);
+            Name = "SelezioneAereoPerBiglietto";
+            Text = "SelezioneAereoPerBiglietto";
+            ((System.ComponentModel.ISupportInitialize)ListaAereiPerBiglietto_table).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView ListaAereiPerBiglietto_table;
+        private Button SelezioneVolo_button;
+        private Button Annulla_button;
+    }
+}
