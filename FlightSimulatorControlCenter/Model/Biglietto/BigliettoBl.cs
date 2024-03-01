382781bae8ce1833c11c93cfa0b34ab517803987
Federico Grassi <grfederico10@gmail.com>
2024-03-01 15:15:07 +0100

    Aggiornato bigliettobl bigliettoTableModel

---
+++ FlightSimulatorControlCenter/Model/Biglietto/BigliettoBl.cs
@@ -0,0 +1,37 @@
+﻿using FlightSimulatorControlCenter.Model.Aereo;
+using FlightSimulatorControlCenter.Model.Flotta;
+using FlightSimulatorControlCenter.Model.Volo;
+using MyNamespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorControlCenter.Model.Biglietto
+{
+    internal class BigliettoBl
+    {
+
+        public int BigliettoId { get; set; }
+        public VoloBl Volo { get; set; }
+        public int PostiDaPrenotare { get; set; }
+        public decimal ImportoTotale { get; set; }
+        public DateTime DataAcquisto { get; set; }
+
+        public BigliettoBl(int bigliettoId, VoloBl voloBl, int postiDaPrenotare, decimal importoTotale, DateTime dataAcquisto)
+        {
+
+            BigliettoId = bigliettoId;
+            Volo = voloBl;
+            PostiDaPrenotare = postiDaPrenotare;
+            ImportoTotale = importoTotale;
+            DataAcquisto = dataAcquisto;
+        }
+
+        public static BigliettoBl BigliettoBlFactory(int bigliettoId, VoloBl voloBl, int postiDaPrenotare, decimal importoTotale, DateTime dataAcquisto)
+        {
+            return new BigliettoBl(bigliettoId, voloBl, postiDaPrenotare, importoTotale, dataAcquisto);
+        }
+    }
+}
---
+++ FlightSimulatorControlCenter/Model/Biglietto/BigliettoTableModel.cs
@@ -0,0 +1,33 @@
+﻿using FlightSimulatorControlCenter.Model.Flotta;
+using FlightSimulatorControlCenter.Model.Volo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorControlCenter.Model.Biglietto
+{
+    internal class BigliettoTableModel
+    {
+        public long BigliettoId {  get; set; }
+        public long VoloId { get; set; }
+        public int PostiDaPrenotare { get; set; }
+        public decimal ImportoTotale { get; set; }
+        public DateTime DataAcquisto { get; set; }
+
+        public BigliettoTableModel(long bigliettoId, long voloId, int postiDaPrenotare, decimal importoTotale, DateTime dataAcquisto)
+        { 
+            BigliettoId = bigliettoId;
+            VoloId = voloId;
+            PostiDaPrenotare = postiDaPrenotare;
+            ImportoTotale = importoTotale;
+            DataAcquisto = dataAcquisto;
+        }
+
+        public static BigliettoTableModel FlottaTableModelFactory(long bigliettoId, long voloId, int postiDaPrenotare, decimal importoTotale, DateTime dataAcquisto)
+        { 
+            return new BigliettoTableModel(bigliettoId, voloId, postiDaPrenotare, importoTotale, dataAcquisto);
+        }
+    }
+}
